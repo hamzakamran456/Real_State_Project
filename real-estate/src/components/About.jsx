37cbd3207fe3a4122839159307a60217517328a2
Hamza Kamran <hamzakamrn8@gmail.com>
2025-03-20 15:07:05 +0500

    create about page

---
+++ real-estate/src/components/About.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const About = () => {
+  return (
+    <div
+      className="flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden"
+      id="About"
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2">
+        About{" "}
+        <span className="underline underline-offset-4 decoration-1 under font-light">
+          Our Brand
+        </span>
+      </h1>
+      <p className="text-gray-500 max-w-80 text-center mb-8">
+        Passionate About Properties, Dedicated to Your Vision
+      </p>
+      <div className="flex flex-col md:flex-row items-center md:items-start md:gap-20">
+        <img
+          src={assets.brand_img}
+          className="w-full md:w-1/2 max-w-lg"
+          alt=""
+        />
+        <div className="flex flex-col items-center md:items-start mt-10 text-gray-600">
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
