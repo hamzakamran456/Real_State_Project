8ae9a231d4941d0f7d1a85232c12b45bef3f4d0c
Hamza Kamran <hamzakamrn8@gmail.com>
2025-03-20 14:50:27 +0500

    app set up header and navbar complete

---
+++ real-estate/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import headerImg from "../assets/header_img.png";
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <div
+      className="min-h-screen w-full mb-4 bg-cover bg-center flex items-center overflow-hidden"
+      style={{ backgroundImage: `url(${headerImg})` }}
+      id="Header"
+    >
+      <Navbar />
+      <div className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white">
+        <h2 className="text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20">
+          Explore homes that fit your dreams
+        </h2>
+        <div className="space-x-6 mt-16">
+          <a
+            href="#Projects"
+            className="border border-solid border-white px-8 py-3 rounded"
+          >
+            Projects
+          </a>
+          <a href="#Contact" className="bg-blue-500 px-8 py-3 rounded">
+            Contact Us
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
