f380ccb325bc7a58ab1e39beedb866e05cd2ab5b
Hamza Kamran <hamzakamrn8@gmail.com>
2025-04-03 17:23:17 +0500

    all section complete

--- real-estate/src/App.jsx
+++ real-estate/src/App.jsx
@@ -3,14 +3,19 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Project from "./components/Project";
 import Testimonial from "./components/Testimonial";
+import Contact from "./components/Contact";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <div>
+      <ToastContainer />
       <Header />
       <About />
       <Project />
       <Testimonial />
+      <Contact />
     </div>
   );
 };
--- real-estate/src/components/Testimonial.jsx
+++ real-estate/src/components/Testimonial.jsx
@@ -1,12 +1,45 @@
 import React from "react";
+import { assets, testimonialsData } from "../assets/assets";
 
 const Testimonial = () => {
   return (
-    <div>
-      <h1>
-        Customer <span>Testimonials</span>
+    <div
+      className="container mx-auto py-10 lg:px-32 w-full overflow-hidden"
+      id="Testimonials"
+    >
+      <h1 className="text-center text-2xl sm:text-4xl font-bold mb-2">
+        Customer{" "}
+        <span className="underline underline-offset-4 decoration-1 under font-light">
+          Testimonials
+        </span>
       </h1>
-      <p>Real Stories from Those Who Found Home with Us</p>
+      <p className="text-gray-500 max-w-80 text-center mb-8 mx-auto">
+        Real Stories from Those Who Found Home with Us
+      </p>
+      <div className="flex flex-wrap justify-center gap-8">
+        {testimonialsData.map((testimonial, index) => (
+          <div
+            key={index}
+            className="max-w-[300px] border shadow-lg rounded px-8 py-10 text-center"
+          >
+            <img
+              className="w-20 h-20 rounded-full mx-auto mb-4"
+              src={testimonial.image}
+              alt={testimonial.alt}
+            />
+            <h2 className="text-xl text-gray-700 font-medium">
+              {testimonial.name}
+            </h2>
+            <p className="text-sm text-gray-500 mb-4">{testimonial.title}</p>
+            <div className="flex justify-center gap-1 text-red-500 mb-4">
+              {Array.from({ length: testimonial.rating }, (item, index) => (
+                <img key={index} src={assets.star_icon} alt="" />
+              ))}
+            </div>
+            <p className="text-gray-600">{testimonial.text}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
